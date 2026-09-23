b1aad67bd52ded11c55d8c624519483374d9c263
AlexanderLH57 <alexander@vefsida.is>
2026-09-23 20:04:12 +0000

    update

--- js/app.js
+++ js/app.js
@@ -4,3 +4,41 @@ const qwerty = document.getElementById("qwerty");
 const word = document.getElementById("word");
 
 let missed = 0;
+
+const setupForm = document.getElementById("setup-form");
+const difficultySelect = document.getElementById("difficulty");
+const lifeIconSelect = document.getElementById("lifeicon");
+const overlay = document.getElementById("overlay");
+
+let selectedDifficulty = "";
+let selectedLifeIcon = "";
+
+async function getRandomWordAsArray() {
+  const response = await fetch("./answers.json");
+  const answers = await response.json();
+
+  const words = answers[selectedDifficulty];
+
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const randomWord = words[randomIndex];
+
+  return randomWord.split("");
+}
+
+setupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  selectedDifficulty = difficultySelect.value;
+  selectedLifeIcon = lifeIconSelect.value;
+
+  const lifeImages = document.querySelectorAll(".tries img");
+
+lifeImages.forEach((img) => {
+  img.src = `./images/live${selectedLifeIcon}.png`;
+});
+
+  overlay.style.display = "none";
+
+  console.log("Difficulty:", selectedDifficulty);
+  console.log("Life icon:", selectedLifeIcon);
+});
